f0dbd4b86cb21ec0d7ecf33d742e49afcb360e93
Mayur Duduka <100664505+MayurDuduka@users.noreply.github.com>
2024-06-20 15:15:59 +0530

    Update star-server.js

--- star-server.js
+++ star-server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const { createCanvas } = require('canvas');
+require('dotenv').config();
 const app = express();
 const port = 3000;
 
