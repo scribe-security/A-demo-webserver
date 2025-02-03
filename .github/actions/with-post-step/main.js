27b0245c6b034f6dd8258d73607de078b37d486c
houdini91 <mdstrauss91@gmail.com>
2025-02-03 14:06:02 +0200

    test

--- .github/actions/with-post-step/main.js
+++ .github/actions/with-post-step/main.js
@@ -4,11 +4,14 @@ const { appendFileSync } = require("fs");
 const { EOL } = require("os");
 
 function run(cmd) {
-  const subprocess = spawn(cmd, { stdio: "inherit", shell: true });
-  subprocess.on("exit", (exitCode) => {
-    process.exitCode = exitCode;
-  });
-}
+    const subprocess = spawn(cmd, {
+      stdio: "inherit",
+      shell: "/usr/bin/bash"  // or just "bash"
+    });
+    subprocess.on("exit", (exitCode) => {
+      process.exitCode = exitCode;
+    });
+  }
 
 const key = process.env.INPUT_KEY.toUpperCase();
 
